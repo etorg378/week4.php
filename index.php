13a058e0d32ef2374797cdc3d3c3e075a9bed811
etorg378 <45890149+etorg378@users.noreply.github.com>
2020-09-23 22:11:58 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"en\">\n";
+echo "\n";
+echo "<head>\n";
+echo "  <meta charset=\"utf-8\">\n";
+echo "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n";
+echo "  <title>CNMT 310 Assignment 1</title>\n";
+echo "  <link href=\"css/styles.css\" rel=\"stylesheet\" type=\"text/css\">\n";
+echo "  <script src=\"hello.js\"></script>\n";
+echo "\n";
+echo "\n";
+echo "<body>\n";
+echo "    <h1>Evan J. Torgerson</h1>\n";
+echo "</body>\n";
+echo "\n";
+echo "</html>";
+
+?>
